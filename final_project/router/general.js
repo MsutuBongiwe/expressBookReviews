034a75d9fdf6ac3763201ea2c7f17f9d09c1479c
MsutuBongiwe <bongiwe@muntumi.com>
2026-05-20 13:23:19 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,5 +39,49 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+const axios = require('axios');
 
+// Task 10: Get all books using Async/Await
+public_users.get('/async-books', async (req, res) => {
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching books", error: error.message });
+  }
+});
+
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/promise-isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+      return res.status(200).json(response.data);
+    })
+    .catch(error => {
+      return res.status(500).json({ message: "Error fetching book", error: error.message });
+    });
+});
+
+// Task 12: Get book details based on Author using Async/Await
+public_users.get('/async-author/:author', async (req, res) => {
+  const author = req.params.author;
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching author", error: error.message });
+  }
+});
+
+// Task 13: Get book details based on Title using Async/Await
+public_users.get('/async-title/:title', async (req, res) => {
+  const title = req.params.title;
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching title", error: error.message });
+  }
+});
 module.exports.general = public_users;
